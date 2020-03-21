6f01a76e876ab1d64d785f6e2aa53a8568b3b3b2
maricotagc <36520845+maricotagc@users.noreply.github.com>
2020-03-21 13:21:35 +0100

    Create Config.java

---
+++ codeExamples/Config.java
@@ -0,0 +1,6 @@
+public class Config {
+
+    public static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+    public static final String WEBDRIVER_CHROME_DRIVER_PATH = "C://Users//maric//IdeaProjects//raphaGit//drivers//chromedriver.exe";
+    
+}
