e2891515da361875103cea8e3442430bb024bd90
maricotagc <36520845+maricotagc@users.noreply.github.com>
2020-06-21 10:57:51 +0200

    Create MaximizeBrowser.java

---
+++ codeExamples/MaximizeBrowser.java
@@ -0,0 +1,27 @@
+package org.moita.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.moita.utilities.Config.*;
+
+public class DriverFactory {
+
+    public static WebDriver creation() {
+        ChromeOptions options = new ChromeOptions();
+        //For executing test in specific browser language
+        options.addArguments(LANG_EN);
+
+        //Instance of the create test will use in every action
+        WebDriver driver = new ChromeDriver(options);
+
+        //Windows maximization and timeout
+        driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}
